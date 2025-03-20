d39e70cbc3f69807aeb357c340885c5d0fe65c8b
ansanych <ansanych@yandex.ru>
2025-03-20 13:41:16 +0500

    pricer history

--- api_v2/pricer_v2.pb.go
+++ api_v2/pricer_v2.pb.go
@@ -122,7 +122,7 @@ func (x *ParamsForCounter) GetParams() []*ParamForCounter {
 	return nil
 }
 
-type ProductPricerHistory struct {
+type ShopProductPricerHistory struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
@@ -135,10 +135,93 @@ type ProductPricerHistory struct {
 	Error      bool   `protobuf:"varint,6,opt,name=error,proto3" json:"error,omitempty"`
 }
 
+func (x *ShopProductPricerHistory) Reset() {
+	*x = ShopProductPricerHistory{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_pricer_v2_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *ShopProductPricerHistory) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ShopProductPricerHistory) ProtoMessage() {}
+
+func (x *ShopProductPricerHistory) ProtoReflect() protoreflect.Message {
+	mi := &file_pricer_v2_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ShopProductPricerHistory.ProtoReflect.Descriptor instead.
+func (*ShopProductPricerHistory) Descriptor() ([]byte, []int) {
+	return file_pricer_v2_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *ShopProductPricerHistory) GetUpdated() string {
+	if x != nil {
+		return x.Updated
+	}
+	return ""
+}
+
+func (x *ShopProductPricerHistory) GetShopPrice() int32 {
+	if x != nil {
+		return x.ShopPrice
+	}
+	return 0
+}
+
+func (x *ShopProductPricerHistory) GetParsePrice() int32 {
+	if x != nil {
+		return x.ParsePrice
+	}
+	return 0
+}
+
+func (x *ShopProductPricerHistory) GetSetPrice() int32 {
+	if x != nil {
+		return x.SetPrice
+	}
+	return 0
+}
+
+func (x *ShopProductPricerHistory) GetSendPrice() bool {
+	if x != nil {
+		return x.SendPrice
+	}
+	return false
+}
+
+func (x *ShopProductPricerHistory) GetError() bool {
+	if x != nil {
+		return x.Error
+	}
+	return false
+}
+
+type ProductPricerHistory struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	ShopProduct *ShopProduct                `protobuf:"bytes,1,opt,name=shopProduct,proto3" json:"shopProduct,omitempty"`
+	Data        []*ShopProductPricerHistory `protobuf:"bytes,2,rep,name=data,proto3" json:"data,omitempty"`
+}
+
 func (x *ProductPricerHistory) Reset() {
 	*x = ProductPricerHistory{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_pricer_v2_proto_msgTypes[2]
+		mi := &file_pricer_v2_proto_msgTypes[3]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -151,7 +234,7 @@ func (x *ProductPricerHistory) String() string {
 func (*ProductPricerHistory) ProtoMessage() {}
 
 func (x *ProductPricerHistory) ProtoReflect() protoreflect.Message {
-	mi := &file_pricer_v2_proto_msgTypes[2]
+	mi := &file_pricer_v2_proto_msgTypes[3]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -164,49 +247,21 @@ func (x *ProductPricerHistory) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use ProductPricerHistory.ProtoReflect.Descriptor instead.
 func (*ProductPricerHistory) Descriptor() ([]byte, []int) {
-	return file_pricer_v2_proto_rawDescGZIP(), []int{2}
+	return file_pricer_v2_proto_rawDescGZIP(), []int{3}
 }
 
-func (x *ProductPricerHistory) GetUpdated() string {
+func (x *ProductPricerHistory) GetShopProduct() *ShopProduct {
 	if x != nil {
-		return x.Updated
+		return x.ShopProduct
 	}
-	return ""
+	return nil
 }
 
-func (x *ProductPricerHistory) GetShopPrice() int32 {
+func (x *ProductPricerHistory) GetData() []*ShopProductPricerHistory {
 	if x != nil {
-		return x.ShopPrice
+		return x.Data
 	}
-	return 0
-}
-
-func (x *ProductPricerHistory) GetParsePrice() int32 {
-	if x != nil {
-		return x.ParsePrice
-	}
-	return 0
-}
-
-func (x *ProductPricerHistory) GetSetPrice() int32 {
-	if x != nil {
-		return x.SetPrice
-	}
-	return 0
-}
-
-func (x *ProductPricerHistory) GetSendPrice() bool {
-	if x != nil {
-		return x.SendPrice
-	}
-	return false
-}
-
-func (x *ProductPricerHistory) GetError() bool {
-	if x != nil {
-		return x.Error
-	}
-	return false
+	return nil
 }
 
 type ProductPricer struct {
@@ -228,7 +283,7 @@ type ProductPricer struct {
 func (x *ProductPricer) Reset() {
 	*x = ProductPricer{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_pricer_v2_proto_msgTypes[3]
+		mi := &file_pricer_v2_proto_msgTypes[4]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -241,7 +296,7 @@ func (x *ProductPricer) String() string {
 func (*ProductPricer) ProtoMessage() {}
 
 func (x *ProductPricer) ProtoReflect() protoreflect.Message {
-	mi := &file_pricer_v2_proto_msgTypes[3]
+	mi := &file_pricer_v2_proto_msgTypes[4]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -254,7 +309,7 @@ func (x *ProductPricer) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use ProductPricer.ProtoReflect.Descriptor instead.
 func (*ProductPricer) Descriptor() ([]byte, []int) {
-	return file_pricer_v2_proto_rawDescGZIP(), []int{3}
+	return file_pricer_v2_proto_rawDescGZIP(), []int{4}
 }
 
 func (x *ProductPricer) GetProductID() int64 {
@@ -331,7 +386,7 @@ type ProductPricers struct {
 func (x *ProductPricers) Reset() {
 	*x = ProductPricers{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_pricer_v2_proto_msgTypes[4]
+		mi := &file_pricer_v2_proto_msgTypes[5]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -344,7 +399,7 @@ func (x *ProductPricers) String() string {
 func (*ProductPricers) ProtoMessage() {}
 
 func (x *ProductPricers) ProtoReflect() protoreflect.Message {
-	mi := &file_pricer_v2_proto_msgTypes[4]
+	mi := &file_pricer_v2_proto_msgTypes[5]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -357,7 +412,7 @@ func (x *ProductPricers) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use ProductPricers.ProtoReflect.Descriptor instead.
 func (*ProductPricers) Descriptor() ([]byte, []int) {
-	return file_pricer_v2_proto_rawDescGZIP(), []int{4}
+	return file_pricer_v2_proto_rawDescGZIP(), []int{5}
 }
 
 func (x *ProductPricers) GetData() []*ProductPricer {
@@ -381,19 +436,28 @@ var file_pricer_v2_proto_rawDesc = []byte{
 	0x43, 0x6f, 0x75, 0x6e, 0x74, 0x65, 0x72, 0x12, 0x32, 0x0a, 0x06, 0x70, 0x61, 0x72, 0x61, 0x6d,
 	0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x1a, 0x2e, 0x63, 0x65, 0x72, 0x61, 0x73, 0x75,
 	0x73, 0x56, 0x32, 0x2e, 0x50, 0x61, 0x72, 0x61, 0x6d, 0x46, 0x6f, 0x72, 0x43, 0x6f, 0x75, 0x6e,
-	0x74, 0x65, 0x72, 0x52, 0x06, 0x70, 0x61, 0x72, 0x61, 0x6d, 0x73, 0x22, 0xbe, 0x01, 0x0a, 0x14,
-	0x50, 0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x50, 0x72, 0x69, 0x63, 0x65, 0x72, 0x48, 0x69, 0x73,
-	0x74, 0x6f, 0x72, 0x79, 0x12, 0x18, 0x0a, 0x07, 0x75, 0x70, 0x64, 0x61, 0x74, 0x65, 0x64, 0x18,
-	0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x75, 0x70, 0x64, 0x61, 0x74, 0x65, 0x64, 0x12, 0x1c,
-	0x0a, 0x09, 0x73, 0x68, 0x6f, 0x70, 0x50, 0x72, 0x69, 0x63, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28,
-	0x05, 0x52, 0x09, 0x73, 0x68, 0x6f, 0x70, 0x50, 0x72, 0x69, 0x63, 0x65, 0x12, 0x1e, 0x0a, 0x0a,
-	0x70, 0x61, 0x72, 0x73, 0x65, 0x50, 0x72, 0x69, 0x63, 0x65, 0x18, 0x03, 0x20, 0x01, 0x28, 0x05,
-	0x52, 0x0a, 0x70, 0x61, 0x72, 0x73, 0x65, 0x50, 0x72, 0x69, 0x63, 0x65, 0x12, 0x1a, 0x0a, 0x08,
-	0x73, 0x65, 0x74, 0x50, 0x72, 0x69, 0x63, 0x65, 0x18, 0x04, 0x20, 0x01, 0x28, 0x05, 0x52, 0x08,
-	0x73, 0x65, 0x74, 0x50, 0x72, 0x69, 0x63, 0x65, 0x12, 0x1c, 0x0a, 0x09, 0x73, 0x65, 0x6e, 0x64,
-	0x50, 0x72, 0x69, 0x63, 0x65, 0x18, 0x05, 0x20, 0x01, 0x28, 0x08, 0x52, 0x09, 0x73, 0x65, 0x6e,
-	0x64, 0x50, 0x72, 0x69, 0x63, 0x65, 0x12, 0x14, 0x0a, 0x05, 0x65, 0x72, 0x72, 0x6f, 0x72, 0x18,
-	0x06, 0x20, 0x01, 0x28, 0x08, 0x52, 0x05, 0x65, 0x72, 0x72, 0x6f, 0x72, 0x22, 0xa0, 0x02, 0x0a,
+	0x74, 0x65, 0x72, 0x52, 0x06, 0x70, 0x61, 0x72, 0x61, 0x6d, 0x73, 0x22, 0xc2, 0x01, 0x0a, 0x18,
+	0x53, 0x68, 0x6f, 0x70, 0x50, 0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x50, 0x72, 0x69, 0x63, 0x65,
+	0x72, 0x48, 0x69, 0x73, 0x74, 0x6f, 0x72, 0x79, 0x12, 0x18, 0x0a, 0x07, 0x75, 0x70, 0x64, 0x61,
+	0x74, 0x65, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x75, 0x70, 0x64, 0x61, 0x74,
+	0x65, 0x64, 0x12, 0x1c, 0x0a, 0x09, 0x73, 0x68, 0x6f, 0x70, 0x50, 0x72, 0x69, 0x63, 0x65, 0x18,
+	0x02, 0x20, 0x01, 0x28, 0x05, 0x52, 0x09, 0x73, 0x68, 0x6f, 0x70, 0x50, 0x72, 0x69, 0x63, 0x65,
+	0x12, 0x1e, 0x0a, 0x0a, 0x70, 0x61, 0x72, 0x73, 0x65, 0x50, 0x72, 0x69, 0x63, 0x65, 0x18, 0x03,
+	0x20, 0x01, 0x28, 0x05, 0x52, 0x0a, 0x70, 0x61, 0x72, 0x73, 0x65, 0x50, 0x72, 0x69, 0x63, 0x65,
+	0x12, 0x1a, 0x0a, 0x08, 0x73, 0x65, 0x74, 0x50, 0x72, 0x69, 0x63, 0x65, 0x18, 0x04, 0x20, 0x01,
+	0x28, 0x05, 0x52, 0x08, 0x73, 0x65, 0x74, 0x50, 0x72, 0x69, 0x63, 0x65, 0x12, 0x1c, 0x0a, 0x09,
+	0x73, 0x65, 0x6e, 0x64, 0x50, 0x72, 0x69, 0x63, 0x65, 0x18, 0x05, 0x20, 0x01, 0x28, 0x08, 0x52,
+	0x09, 0x73, 0x65, 0x6e, 0x64, 0x50, 0x72, 0x69, 0x63, 0x65, 0x12, 0x14, 0x0a, 0x05, 0x65, 0x72,
+	0x72, 0x6f, 0x72, 0x18, 0x06, 0x20, 0x01, 0x28, 0x08, 0x52, 0x05, 0x65, 0x72, 0x72, 0x6f, 0x72,
+	0x22, 0x89, 0x01, 0x0a, 0x14, 0x50, 0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x50, 0x72, 0x69, 0x63,
+	0x65, 0x72, 0x48, 0x69, 0x73, 0x74, 0x6f, 0x72, 0x79, 0x12, 0x38, 0x0a, 0x0b, 0x73, 0x68, 0x6f,
+	0x70, 0x50, 0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x16,
+	0x2e, 0x63, 0x65, 0x72, 0x61, 0x73, 0x75, 0x73, 0x56, 0x32, 0x2e, 0x53, 0x68, 0x6f, 0x70, 0x50,
+	0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x52, 0x0b, 0x73, 0x68, 0x6f, 0x70, 0x50, 0x72, 0x6f, 0x64,
+	0x75, 0x63, 0x74, 0x12, 0x37, 0x0a, 0x04, 0x64, 0x61, 0x74, 0x61, 0x18, 0x02, 0x20, 0x03, 0x28,
+	0x0b, 0x32, 0x23, 0x2e, 0x63, 0x65, 0x72, 0x61, 0x73, 0x75, 0x73, 0x56, 0x32, 0x2e, 0x53, 0x68,
+	0x6f, 0x70, 0x50, 0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x50, 0x72, 0x69, 0x63, 0x65, 0x72, 0x48,
+	0x69, 0x73, 0x74, 0x6f, 0x72, 0x79, 0x52, 0x04, 0x64, 0x61, 0x74, 0x61, 0x22, 0xa0, 0x02, 0x0a,
 	0x0d, 0x50, 0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x50, 0x72, 0x69, 0x63, 0x65, 0x72, 0x12, 0x1c,
 	0x0a, 0x09, 0x70, 0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x49, 0x44, 0x18, 0x01, 0x20, 0x01, 0x28,
 	0x03, 0x52, 0x09, 0x70, 0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x49, 0x44, 0x12, 0x12, 0x0a, 0x04,
@@ -474,53 +538,57 @@ func file_pricer_v2_proto_rawDescGZIP() []byte {
 	return file_pricer_v2_proto_rawDescData
 }
 
-var file_pricer_v2_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
+var file_pricer_v2_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
 var file_pricer_v2_proto_goTypes = []interface{}{
-	(*ParamForCounter)(nil),      // 0: cerasusV2.ParamForCounter
-	(*ParamsForCounter)(nil),     // 1: cerasusV2.ParamsForCounter
-	(*ProductPricerHistory)(nil), // 2: cerasusV2.ProductPricerHistory
-	(*ProductPricer)(nil),        // 3: cerasusV2.ProductPricer
-	(*ProductPricers)(nil),       // 4: cerasusV2.ProductPricers
-	(*PingRequest)(nil),          // 5: cerasusV2.PingRequest
-	(*Company)(nil),              // 6: cerasusV2.Company
-	(*ParserGetRequest)(nil),     // 7: cerasusV2.ParserGetRequest
-	(*ParserSetRequest)(nil),     // 8: cerasusV2.ParserSetRequest
-	(*DetectorGetRequest)(nil),   // 9: cerasusV2.DetectorGetRequest
-	(*DetectorSetRequest)(nil),   // 10: cerasusV2.DetectorSetRequest
-	(*RequestByID)(nil),          // 11: cerasusV2.RequestByID
-	(*PingReply)(nil),            // 12: cerasusV2.PingReply
-	(*ParserJob)(nil),            // 13: cerasusV2.ParserJob
-	(*StatusReply)(nil),          // 14: cerasusV2.StatusReply
-	(*DetectorGetReply)(nil),     // 15: cerasusV2.DetectorGetReply
-	(*ReplyID)(nil),              // 16: cerasusV2.ReplyID
+	(*ParamForCounter)(nil),          // 0: cerasusV2.ParamForCounter
+	(*ParamsForCounter)(nil),         // 1: cerasusV2.ParamsForCounter
+	(*ShopProductPricerHistory)(nil), // 2: cerasusV2.ShopProductPricerHistory
+	(*ProductPricerHistory)(nil),     // 3: cerasusV2.ProductPricerHistory
+	(*ProductPricer)(nil),            // 4: cerasusV2.ProductPricer
+	(*ProductPricers)(nil),           // 5: cerasusV2.ProductPricers
+	(*ShopProduct)(nil),              // 6: cerasusV2.ShopProduct
+	(*PingRequest)(nil),              // 7: cerasusV2.PingRequest
+	(*Company)(nil),                  // 8: cerasusV2.Company
+	(*ParserGetRequest)(nil),         // 9: cerasusV2.ParserGetRequest
+	(*ParserSetRequest)(nil),         // 10: cerasusV2.ParserSetRequest
+	(*DetectorGetRequest)(nil),       // 11: cerasusV2.DetectorGetRequest
+	(*DetectorSetRequest)(nil),       // 12: cerasusV2.DetectorSetRequest
+	(*RequestByID)(nil),              // 13: cerasusV2.RequestByID
+	(*PingReply)(nil),                // 14: cerasusV2.PingReply
+	(*ParserJob)(nil),                // 15: cerasusV2.ParserJob
+	(*StatusReply)(nil),              // 16: cerasusV2.StatusReply
+	(*DetectorGetReply)(nil),         // 17: cerasusV2.DetectorGetReply
+	(*ReplyID)(nil),                  // 18: cerasusV2.ReplyID
 }
 var file_pricer_v2_proto_depIdxs = []int32{
 	0,  // 0: cerasusV2.ParamsForCounter.params:type_name -> cerasusV2.ParamForCounter
-	2,  // 1: cerasusV2.ProductPricer.history:type_name -> cerasusV2.ProductPricerHistory
-	3,  // 2: cerasusV2.ProductPricers.data:type_name -> cerasusV2.ProductPricer
-	5,  // 3: cerasusV2.Pricer.Ping:input_type -> cerasusV2.PingRequest
-	6,  // 4: cerasusV2.Pricer.GetParamsForCounter:input_type -> cerasusV2.Company
-	7,  // 5: cerasusV2.Pricer.GetParserData:input_type -> cerasusV2.ParserGetRequest
-	8,  // 6: cerasusV2.Pricer.SetParserData:input_type -> cerasusV2.ParserSetRequest
-	9,  // 7: cerasusV2.Pricer.GetDetectorData:input_type -> cerasusV2.DetectorGetRequest
-	10, // 8: cerasusV2.Pricer.SetDetectorData:input_type -> cerasusV2.DetectorSetRequest
-	6,  // 9: cerasusV2.Pricer.GetProductsWithPricer:input_type -> cerasusV2.Company
-	11, // 10: cerasusV2.Pricer.GetProductPricers:input_type -> cerasusV2.RequestByID
-	3,  // 11: cerasusV2.Pricer.SetProductPricer:input_type -> cerasusV2.ProductPricer
-	12, // 12: cerasusV2.Pricer.Ping:output_type -> cerasusV2.PingReply
-	1,  // 13: cerasusV2.Pricer.GetParamsForCounter:output_type -> cerasusV2.ParamsForCounter
-	13, // 14: cerasusV2.Pricer.GetParserData:output_type -> cerasusV2.ParserJob
-	14, // 15: cerasusV2.Pricer.SetParserData:output_type -> cerasusV2.StatusReply
-	15, // 16: cerasusV2.Pricer.GetDetectorData:output_type -> cerasusV2.DetectorGetReply
-	14, // 17: cerasusV2.Pricer.SetDetectorData:output_type -> cerasusV2.StatusReply
-	16, // 18: cerasusV2.Pricer.GetProductsWithPricer:output_type -> cerasusV2.ReplyID
-	4,  // 19: cerasusV2.Pricer.GetProductPricers:output_type -> cerasusV2.ProductPricers
-	14, // 20: cerasusV2.Pricer.SetProductPricer:output_type -> cerasusV2.StatusReply
-	12, // [12:21] is the sub-list for method output_type
-	3,  // [3:12] is the sub-list for method input_type
-	3,  // [3:3] is the sub-list for extension type_name
-	3,  // [3:3] is the sub-list for extension extendee
-	0,  // [0:3] is the sub-list for field type_name
+	6,  // 1: cerasusV2.ProductPricerHistory.shopProduct:type_name -> cerasusV2.ShopProduct
+	2,  // 2: cerasusV2.ProductPricerHistory.data:type_name -> cerasusV2.ShopProductPricerHistory
+	3,  // 3: cerasusV2.ProductPricer.history:type_name -> cerasusV2.ProductPricerHistory
+	4,  // 4: cerasusV2.ProductPricers.data:type_name -> cerasusV2.ProductPricer
+	7,  // 5: cerasusV2.Pricer.Ping:input_type -> cerasusV2.PingRequest
+	8,  // 6: cerasusV2.Pricer.GetParamsForCounter:input_type -> cerasusV2.Company
+	9,  // 7: cerasusV2.Pricer.GetParserData:input_type -> cerasusV2.ParserGetRequest
+	10, // 8: cerasusV2.Pricer.SetParserData:input_type -> cerasusV2.ParserSetRequest
+	11, // 9: cerasusV2.Pricer.GetDetectorData:input_type -> cerasusV2.DetectorGetRequest
+	12, // 10: cerasusV2.Pricer.SetDetectorData:input_type -> cerasusV2.DetectorSetRequest
+	8,  // 11: cerasusV2.Pricer.GetProductsWithPricer:input_type -> cerasusV2.Company
+	13, // 12: cerasusV2.Pricer.GetProductPricers:input_type -> cerasusV2.RequestByID
+	4,  // 13: cerasusV2.Pricer.SetProductPricer:input_type -> cerasusV2.ProductPricer
+	14, // 14: cerasusV2.Pricer.Ping:output_type -> cerasusV2.PingReply
+	1,  // 15: cerasusV2.Pricer.GetParamsForCounter:output_type -> cerasusV2.ParamsForCounter
+	15, // 16: cerasusV2.Pricer.GetParserData:output_type -> cerasusV2.ParserJob
+	16, // 17: cerasusV2.Pricer.SetParserData:output_type -> cerasusV2.StatusReply
+	17, // 18: cerasusV2.Pricer.GetDetectorData:output_type -> cerasusV2.DetectorGetReply
+	16, // 19: cerasusV2.Pricer.SetDetectorData:output_type -> cerasusV2.StatusReply
+	18, // 20: cerasusV2.Pricer.GetProductsWithPricer:output_type -> cerasusV2.ReplyID
+	5,  // 21: cerasusV2.Pricer.GetProductPricers:output_type -> cerasusV2.ProductPricers
+	16, // 22: cerasusV2.Pricer.SetProductPricer:output_type -> cerasusV2.StatusReply
+	14, // [14:23] is the sub-list for method output_type
+	5,  // [5:14] is the sub-list for method input_type
+	5,  // [5:5] is the sub-list for extension type_name
+	5,  // [5:5] is the sub-list for extension extendee
+	0,  // [0:5] is the sub-list for field type_name
 }
 
 func init() { file_pricer_v2_proto_init() }
@@ -555,7 +623,7 @@ func file_pricer_v2_proto_init() {
 			}
 		}
 		file_pricer_v2_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*ProductPricerHistory); i {
+			switch v := v.(*ShopProductPricerHistory); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -567,7 +635,7 @@ func file_pricer_v2_proto_init() {
 			}
 		}
 		file_pricer_v2_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*ProductPricer); i {
+			switch v := v.(*ProductPricerHistory); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -579,6 +647,18 @@ func file_pricer_v2_proto_init() {
 			}
 		}
 		file_pricer_v2_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*ProductPricer); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_pricer_v2_proto_msgTypes[5].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*ProductPricers); i {
 			case 0:
 				return &v.state
@@ -597,7 +677,7 @@ func file_pricer_v2_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_pricer_v2_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   5,
+			NumMessages:   6,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
