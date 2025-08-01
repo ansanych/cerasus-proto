e4229f4114cac24949472f5df71d86be54bb3158
ansanych <ansanych@yandex.ru>
2025-08-01 12:07:18 +0500

    prod counts

--- api_v3/productsV3_grpc.pb.go
+++ api_v3/productsV3_grpc.pb.go
@@ -39,6 +39,9 @@ type ProductsClient interface {
 	DeleteProductLink(ctx context.Context, in *RequestByID, opts ...grpc.CallOption) (*StatusReply, error)
 	GetProductsUnsortedList(ctx context.Context, in *SearchRequest, opts ...grpc.CallOption) (*ShopProductList, error)
 	GetProductsByIDS(ctx context.Context, in *RequestByIDS, opts ...grpc.CallOption) (*ProductList, error)
+	GetShopProductsAppCount(ctx context.Context, in *RequestByShop, opts ...grpc.CallOption) (*Count, error)
+	GetShopProductsUnsortedCount(ctx context.Context, in *RequestByShop, opts ...grpc.CallOption) (*Count, error)
+	GetShopProductsCount(ctx context.Context, in *RequestByShop, opts ...grpc.CallOption) (*Count, error)
 }
 
 type productsClient struct {
@@ -202,6 +205,33 @@ func (c *productsClient) GetProductsByIDS(ctx context.Context, in *RequestByIDS,
 	return out, nil
 }
 
+func (c *productsClient) GetShopProductsAppCount(ctx context.Context, in *RequestByShop, opts ...grpc.CallOption) (*Count, error) {
+	out := new(Count)
+	err := c.cc.Invoke(ctx, "/cerasusV3.Products/GetShopProductsAppCount", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *productsClient) GetShopProductsUnsortedCount(ctx context.Context, in *RequestByShop, opts ...grpc.CallOption) (*Count, error) {
+	out := new(Count)
+	err := c.cc.Invoke(ctx, "/cerasusV3.Products/GetShopProductsUnsortedCount", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *productsClient) GetShopProductsCount(ctx context.Context, in *RequestByShop, opts ...grpc.CallOption) (*Count, error) {
+	out := new(Count)
+	err := c.cc.Invoke(ctx, "/cerasusV3.Products/GetShopProductsCount", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // ProductsServer is the server API for Products service.
 // All implementations must embed UnimplementedProductsServer
 // for forward compatibility
@@ -223,6 +253,9 @@ type ProductsServer interface {
 	DeleteProductLink(context.Context, *RequestByID) (*StatusReply, error)
 	GetProductsUnsortedList(context.Context, *SearchRequest) (*ShopProductList, error)
 	GetProductsByIDS(context.Context, *RequestByIDS) (*ProductList, error)
+	GetShopProductsAppCount(context.Context, *RequestByShop) (*Count, error)
+	GetShopProductsUnsortedCount(context.Context, *RequestByShop) (*Count, error)
+	GetShopProductsCount(context.Context, *RequestByShop) (*Count, error)
 	mustEmbedUnimplementedProductsServer()
 }
 
@@ -280,6 +313,15 @@ func (UnimplementedProductsServer) GetProductsUnsortedList(context.Context, *Sea
 }
 func (UnimplementedProductsServer) GetProductsByIDS(context.Context, *RequestByIDS) (*ProductList, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetProductsByIDS not implemented")
+}
+func (UnimplementedProductsServer) GetShopProductsAppCount(context.Context, *RequestByShop) (*Count, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetShopProductsAppCount not implemented")
+}
+func (UnimplementedProductsServer) GetShopProductsUnsortedCount(context.Context, *RequestByShop) (*Count, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetShopProductsUnsortedCount not implemented")
+}
+func (UnimplementedProductsServer) GetShopProductsCount(context.Context, *RequestByShop) (*Count, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetShopProductsCount not implemented")
 }
 func (UnimplementedProductsServer) mustEmbedUnimplementedProductsServer() {}
 
@@ -600,6 +642,60 @@ func _Products_GetProductsByIDS_Handler(srv interface{}, ctx context.Context, de
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Products_GetShopProductsAppCount_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(RequestByShop)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ProductsServer).GetShopProductsAppCount(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/cerasusV3.Products/GetShopProductsAppCount",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ProductsServer).GetShopProductsAppCount(ctx, req.(*RequestByShop))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Products_GetShopProductsUnsortedCount_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(RequestByShop)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ProductsServer).GetShopProductsUnsortedCount(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/cerasusV3.Products/GetShopProductsUnsortedCount",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ProductsServer).GetShopProductsUnsortedCount(ctx, req.(*RequestByShop))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Products_GetShopProductsCount_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(RequestByShop)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ProductsServer).GetShopProductsCount(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/cerasusV3.Products/GetShopProductsCount",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ProductsServer).GetShopProductsCount(ctx, req.(*RequestByShop))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Products_ServiceDesc is the grpc.ServiceDesc for Products service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -674,6 +770,18 @@ var Products_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetProductsByIDS",
 			Handler:    _Products_GetProductsByIDS_Handler,
+		},
+		{
+			MethodName: "GetShopProductsAppCount",
+			Handler:    _Products_GetShopProductsAppCount_Handler,
+		},
+		{
+			MethodName: "GetShopProductsUnsortedCount",
+			Handler:    _Products_GetShopProductsUnsortedCount_Handler,
+		},
+		{
+			MethodName: "GetShopProductsCount",
+			Handler:    _Products_GetShopProductsCount_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
